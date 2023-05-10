d5dca28819108afd0c08b6cb76bfb68a5c32ffac
Charly Tardy <ctardy@c2r4p5.42nice.fr>
2023-05-10 15:12:12 +0200

    ex01 fix canonic

--- Module04/ex01/Brain.cpp
+++ Module04/ex01/Brain.cpp
@@ -6,19 +6,22 @@
 /*   By: ctardy <ctardy@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 14:20:10 by ctardy            #+#    #+#             */
-/*   Updated: 2023/05/10 14:31:34 by ctardy           ###   ########.fr       */
+/*   Updated: 2023/05/10 15:06:54 by ctardy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Brain.hpp"
 
-Brain::Brain(const Brain &other)
-{
+Brain::Brain(const Brain &other) {
+	for (int i = 0; i < 100; i++) { 
+		ideas[i] = other.ideas[i];
+	}
 	*this = other;
 }
 
-Brain &Brain::operator=(const Brain &other)
-{
+Brain &Brain::operator=(const Brain &other) {
+	for (int i = 0; i < 100; i++) 
+		ideas[i] = other.ideas[i];
 	return (*this);
 }
 
